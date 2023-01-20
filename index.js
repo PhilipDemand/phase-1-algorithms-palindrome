18dc079664338a73d42b0efd304ed83979a0f770
Philip Demand <philipdemand@yahoo.com>
2023-01-20 17:11:31 -0500

    commit tree 42095ad1a8390e6157d1e10f6680c047c6cbd04c

--- index.js
+++ index.js
@@ -1,14 +1,29 @@
 function isPalindrome(word) {
-  // Write your algorithm here
+  let splitString = word.split("");
+  let reverseArray = splitString.reverse();
+  var joinArray = reverseArray.join("")
+  if (word === joinArray) {
+    return true
+  } else {
+    return false
+  }
 }
 
 /* 
-  Add your pseudocode here
+  break up the letters of the word
+  reverse them
+  join them back together
+  see if the word at the beginning matches the word at the end
+  If it does, it's truely a palindrome
 */
 
 /*
-  Add written explanation of your solution here
-*/
+  splitString turns "word" into ["w", "o", "r", "d"]
+  reverse array takes the result of the previous variable and turns it into ["d", "r", "o", "w"]
+  joinArray takes the result of the previous variable and converts the array of strings into a string
+  If the word passed into the function deeply-equals the result of joinArray, the function will return true.
+  Otherwise (if the passed in word does not deeply equal the result of joinArray), this function will return false.
+*/ 
 
 // You can run `node index.js` to view these console logs
 if (require.main === module) {
